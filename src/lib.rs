1ba9a4072da551d67f5136b2dfd58cf67eb27547
Ben Bariteau <ben.bariteau@gmail.com>
2020-11-17 16:29:32 -0800

    add further variants to SegmentType and SegmentValue, for other sized number types

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,19 @@ use serde::de::Visitor;
 use std::fmt::Display;
 
 pub enum SegmentType {
+    F32,
     F64,
+    I8,
+    I16,
+    I32,
+    I64,
     I128,
-    String,
+    U8,
+    U16,
+    U32,
     U64,
+    U128,
+    String,
 }
 
 pub struct SegmentValueSchema {
@@ -22,9 +31,18 @@ pub struct SegmentValueSchema {
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum SegmentValue {
+    F32(f32),
     F64(f64),
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
     I128(i128),
+    U8(u8),
+    U16(u16),
+    U32(u32),
     U64(u64),
+    U128(u128),
     String(String),
 }
 
