6b2a8339b636a7e789cbdb574f6903daae4038dd
Ben Bariteau <ben.bariteau@gmail.com>
2020-11-17 16:06:47 -0800

    implement some serde deserialization

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,11 @@ pub struct SegmentValueSchema {
     pub segment_type: SegmentType,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum SegmentValue {
     F64(f64),
     I128(i128),
-    U128(u128),
+    U64(u64),
     String(String),
 }
 
@@ -51,6 +51,8 @@ pub enum StructPathError {
     ParseIntError(#[from] ParseIntError),
     #[error("Error from serde: {0}")]
     SerdeInternalError(String),
+    #[error("Error is impossible, but reqired structurrally")]
+    Impossible,
 }
 
 impl serde::de::Error for StructPathError {
@@ -77,7 +79,7 @@ fn parse_path_generic(path: String, schema: Schema) -> Result<HashMap<String, Se
                         path_values.insert(segment_value_schema.name.clone(), SegmentValue::I128(segment.parse()?));
                     },
                     SegmentType::UnsignedInteger => {
-                        path_values.insert(segment_value_schema.name.clone(), SegmentValue::U128(segment.parse()?));
+                        path_values.insert(segment_value_schema.name.clone(), SegmentValue::U64(segment.parse()?));
                     },
                     SegmentType::String => {
                         path_values.insert(segment_value_schema.name.clone(), SegmentValue::String(segment.to_owned()));
@@ -93,7 +95,7 @@ struct Deserializer {
     generic_parsed_path: HashMap<String, SegmentValue>,
 }
 
-impl <'de, 'a> serde::de::Deserializer<'de> for &'a mut Deserializer {
+impl <'de, 'a> serde::de::Deserializer<'de> for &'a Deserializer {
     type Error = StructPathError;
 
     fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
@@ -197,6 +199,290 @@ impl <'de, 'a> serde::de::Deserializer<'de> for &'a mut Deserializer {
     }
 
     fn deserialize_struct<V>(self, name: &'static str, fields: &'static [&'static str], visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        visitor.visit_map(MapAccess{
+            deserializer: &self,
+            keys: self.generic_parsed_path.keys().cloned().collect(),
+            last_key: "".to_owned(),
+        })
+    }
+
+    fn deserialize_enum<V>(self, name: &'static str, variants: &'static [&'static str], visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+}
+
+struct MapAccess<'a> {
+    deserializer: &'a Deserializer,
+    keys: Vec<String>,
+    last_key: String,
+}
+
+impl<'de, 'a> serde::de::MapAccess<'de> for MapAccess<'a> {
+    type Error = StructPathError;
+    fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>, Self::Error> where K: serde::de::DeserializeSeed<'de> {
+        if self.keys.len() == 0 {
+            return Ok(None)
+        }
+        {
+            let next_key = &self.keys[0];
+            self.last_key = next_key.clone();
+        }
+        self.keys = self.keys.iter().skip(1).cloned().collect();
+        seed.deserialize(IdentDeserializer{ident: self.last_key.clone()}).map(Some)
+    }
+
+    fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value, Self::Error>  where V: serde::de::DeserializeSeed<'de> {
+        let value = self.deserializer.generic_parsed_path.get(&self.last_key).ok_or(StructPathError::Impossible)?;
+        seed.deserialize(ValueDeserializer{value: value.clone()})
+    }
+}
+
+struct IdentDeserializer{
+    ident: String,
+}
+
+impl <'de> serde::de::Deserializer<'de> for IdentDeserializer {
+    type Error = StructPathError;
+
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        visitor.visit_string(self.ident.clone())
+    }
+
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_unit_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_newtype_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_tuple_struct<V>(self, name: &'static str, len: usize, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_struct<V>(self, name: &'static str, fields: &'static [&'static str], visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_enum<V>(self, name: &'static str, variants: &'static [&'static str], visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        self.deserialize_str(visitor)
+    }
+
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+}
+
+struct ValueDeserializer {
+    value: SegmentValue,
+}
+
+impl <'de, 'a> serde::de::Deserializer<'de> for ValueDeserializer {
+    type Error = StructPathError;
+
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        match self.value {
+            SegmentValue::U64(value) => visitor.visit_u64(value),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        match self.value {
+            SegmentValue::String(value) => visitor.visit_string(value.clone()),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_unit_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_newtype_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_tuple_struct<V>(self, name: &'static str, len: usize, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
+        unimplemented!();
+    }
+
+    fn deserialize_struct<V>(self, name: &'static str, fields: &'static [&'static str], visitor: V) -> Result<V::Value, Self::Error> where V: Visitor<'de> {
         unimplemented!();
     }
 
@@ -215,12 +501,14 @@ impl <'de, 'a> serde::de::Deserializer<'de> for &'a mut Deserializer {
 
 pub fn parse_path<'a, T>(path: String, schema: Schema) -> Result<T, StructPathError> where T: serde::Deserialize<'a> {
     let generic_parsed_path_value = parse_path_generic(path, schema)?;
-    unimplemented!();
+    let mut deserializer = Deserializer{generic_parsed_path: generic_parsed_path_value};
+    T::deserialize(&deserializer)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde::Deserialize;
     #[test]
     fn test_parse_path_generic() {
         assert_eq!(
@@ -242,7 +530,7 @@ mod tests {
                 }
                 ).unwrap(),
                 hashmap!{
-                    "foo".to_owned() => SegmentValue::U128(1),
+                    "foo".to_owned() => SegmentValue::U64(1),
                     "bar".to_owned() => SegmentValue::String("thing".to_owned()),
                 }
             );
@@ -288,5 +576,33 @@ mod tests {
                 "foo".to_owned() => SegmentValue::I128(-1),
             },
             );
+    }
+
+    #[test]
+    fn test_parse_path_basic() {
+        #[derive(Debug, Deserialize, PartialEq)]
+        struct Value{
+            foo: u64,
+            bar: String,
+        }
+
+        let value: Value = parse_path(
+            "/foo/1/bar/thing".to_owned(),
+            Schema{
+                segments: vec![
+                    SegmentSchema::Literal("foo".to_owned()),
+                    SegmentSchema::Value(SegmentValueSchema{
+                        name: "foo".to_owned(),
+                        segment_type: SegmentType::UnsignedInteger,
+                    }),
+                    SegmentSchema::Literal("bar".to_owned()),
+                    SegmentSchema::Value(SegmentValueSchema{
+                        name: "bar".to_owned(),
+                        segment_type: SegmentType::String,
+                    }),
+                ],
+            }
+        ).unwrap();
+        assert_eq!(value, Value{foo: 1, bar: "thing".to_owned()});
     }
 }
